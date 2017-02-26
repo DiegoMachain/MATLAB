e1978ee91d85720f4a470e669002c2c70b0a6c20
DiegoMachain <diego_b_ru@hotmail.com>
2017-02-26 00:14:29 -0600

    Add files via upload

---
+++ LegendrePolynomials.m
@@ -0,0 +1,18 @@
+function[]=LegendrePolynomials()
+degree=9;
+x=-1:0.005:1;x=x';
+f=x;
+p=zeros(length(x),degree+1);
+p(:,1)=1;p(:,2)=f;
+for n=2:degree
+    p(:,n+1)=((2*(n-1)+1)*f.*p(:,n)-(n-1)*p(:,n-1))/(n);
+    figure(1)
+    plot(x,p(:,n+1))
+    ax=gca;
+    ax.XAxisLocation = 'origin';
+    ax.YAxisLocation = 'origin';
+    axis([-1.1 1.1 -1.1 1.1]) 
+    hold on;
+    pause(.2)
+end
+end
